b857b1e843828a7a8dc440cc288590a3c0272add
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-07-12 14:12:54 +0000

    save progress

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+router.get('/profile', function (req, res) {
+    res.render('profile');
+});
+
+
+
 
 module.exports = router;
