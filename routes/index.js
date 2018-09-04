528f93a3d1a64b44402571fe72484ea079114472
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-09-04 13:43:26 +0000

    made home gallery container fluid, fixed login route

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,29 @@ var router = express.Router();
 console.log('routes index');
 
 //show register form
-/*router.get('/register', function (req, res) {
-    // res.send('welcome to the register route');
-    res.render('register');
-});
+// router.get('/register', function (req, res) {
+//     // res.send('welcome to the register route');
+//     res.render('register');
+// });
 
-/////////////handle sign up logic////////////////
-router.post('/register', function (req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-            req.flash('error', err.message);
-            console.log(err);
-            return res.redirect('/register');
-        } else {
-            passport.authenticate('local')(req, res, function () {
-                req.flash('success', 'Welcome to Erics Media ' + user.username);
-                res.redirect('/');
-            })
-        }
-    })
-});
+// /////////////handle sign up logic////////////////
+// router.post('/register', function (req, res) {
+//     var newUser = new User({username: req.body.username});
+//     User.register(newUser, req.body.password, function (err, user) {
+//         if (err) {
+//             req.flash('error', err.message);
+//             console.log(err);
+//             return res.redirect('/register');
+//         } else {
+//             passport.authenticate('local')(req, res, function () {
+//                 req.flash('success', 'Welcome to Erics Media ' + user.username);
+//                 res.redirect('/');
+//             })
+//         }
+//     })
+// });
 
-*/
+
 //show login form
 router.get('/login', function (req, res) {
     res.render('login');
