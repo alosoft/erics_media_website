4d0c728e7fc0a943c938106a9c9b545fcd57aeb0
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-09-04 08:19:05 +0000

    added masonry grid to home page gallery, removed portrait and landscape code from home page

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+var grid = document.querySelector('.grid');
+var msnry;
+
+imagesLoaded( grid, function() {
+  // init Isotope after all images have loaded
+  msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+});
