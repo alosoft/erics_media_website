dcee0934390795145a93816747a0141bf15654c6
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-09-12 17:32:32 +0000

    changed dashboard background image, add middleware to dashboard page, other fixes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,17 +1,18 @@
 let express = require('express');
 let passport = require('passport');
 let Picture = require('../models/picture');
+let middleware = require('../middleware');
 let router = express.Router();
 
 console.log('router dashboard');
 
 // GET / dashboard page
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', middleware.isLoggedIn, (req, res) => {
     res.render('dashboard/home');
 });
 
 // GET / dashboard Picture
-router.get('/dashboard/picture', (req, res) => {
+router.get('/dashboard/picture', middleware.isLoggedIn, (req, res) => {
    Picture.find({}, (err, picture) => {
        if (err) {
            console.log(err);
